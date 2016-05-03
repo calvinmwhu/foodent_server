03ccfbbd324d1bb29b7bbe47f17b0d4c63663b1c
Mingwei Hu <calvinmwhu@gmail.com>
2016-05-02 23:30:58 -0500

    s

--- app/routes.js
+++ app/routes.js
@@ -26,15 +26,57 @@ module.exports = function (router, passport) {
                 }
             });
         } else {
-            document.sort(sort).skip(skip).limit(limit).select(select).exec().then(function (product) {
-                res.status(200).json({message: "OK", data: product});
+            document.sort(sort).skip(skip).limit(limit).select(select).exec().then(function (products) {
+                if (!products) {
+                    res.status(404).json({message: "resources not found", data: []});
+                } else {
+                    res.status(200).json({message: "OK", data: products});
+                }
             }, function (err) {
                 res.status(500).json({message: err.name || err.message || "Unknown Internal Server Error", data: []});
             });
         }
     };
 
+    var getSingleResource = function (model, req, res) {
+        model.findById(req.params.id, function (err, product) {
+            if (err || !product) {
+                res.status(404).json({message: "resource not found", data: []});
+            } else {
+                res.status(200).json({message: "OK", data: product});
+            }
+        });
+    };
+
+    var updateResource = function (model, req, res) {
+        model.findByIdAndUpdate(req.params.id,
+            {$set: req.body},
+            {new: true, runValidators: true},
+            function (err, product) {
+                if (err) {
+                    res.status(500).json({message: err.message || err.name || "Unknown server error", data: []});
+                } else if (!product) {
+                    res.status(404).json({message: "resource not found", data: []});
+                } else {
+                    res.status(200).json({message: "resource updated", data: product});
+                }
+            });
+    };
+
+    var deleteResource = function (model, req, res) {
+        model.findByIdAndRemove(req.params.id,
+            function (err, product) {
+                if (err || !product) {
+                    res.status(404).json({message: "resource not found", data: []});
+                } else {
+                    res.status(200).json({message: "resource deleted", data: product});
+                }
+            });
+    };
+
     router.post('/signup', function (req, res) {
+
+        // later on, will assume req.body has this structure: {user: object, address: object}
         if (!req.body.name || !req.body.email || !req.body.password || !req.body.profileImageUrl) {
             res.status(500).json({message: "Please enter a name, an email, a password, and a image url"});
         } else {
@@ -52,14 +94,6 @@ module.exports = function (router, passport) {
                 var errorMsg = err.name || "Unknown error";
                 res.status(500).json({success: false, message: errorMsg, data: []});
             });
-
-            //newuser.save().then(function (user) {
-            //    var token = jwt.encode({name: user.name, email: user.email}, config.secret);
-            //    res.status(201).json({success: true, message: "User created", data: user, token: 'JWT ' + token});
-            //}, function (err) {
-            //    var errorMsg = err.name || "Unknown error";
-            //    res.status(500).json({success: false, message: errorMsg, data: []});
-            //});
         }
     });
 
@@ -101,7 +135,6 @@ module.exports = function (router, passport) {
         // later on, will assume req.body has this structure: {event: object, invite: object, address: object}
 
         var event = new Event(req.body);
-
         // these two objects are just temporary data for testing purpose
         var invite = new Invite({
             startTime: Date.now(),
@@ -124,7 +157,7 @@ module.exports = function (router, passport) {
             var inviteId = product._id;
             event.invite = inviteId;
             return address.save();
-        }).then(function(product){
+        }).then(function (product) {
             var addressId = product._id;
             event.address = addressId;
             return event.save();
@@ -147,57 +180,33 @@ module.exports = function (router, passport) {
         });
     });
 
-    router.put('/user/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
-        var address = new Address(req.address);
-        var newuser = new User(req.user);
-
-        if (address) {
-            newuser.address = address;
-        }
-        User.findByIdAndUpdate(req.params.id,
-            newuser,
-            {new: true, runValidators: true},
-            function (err, user) {
-                if (err) {
-                    res.status(500).json({message: err.name || err.message || "Unknown server error", data: []});
-                } else if (!user) {
-                    res.status(404).json({message: "User not found", data: []});
-                } else {
-                    res.status(200).json({message: "User updated", data: user});
-                }
-            });
+    router.post('/addresses', passport.authenticate('jwt', {session: false}), function (req, res) {
+        var address = new Address(req.body);
+        address.save().then(function (product) {
+            res.status(201).json({message: "Address Created", data: product});
+        }, function (err) {
+            res.status(500).json({message: err.name || err.message || "Unknown Internal Server Error", data: []});
+        });
     });
 
-    router.put('/event/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
-        var address = new Address(req.address);
-        var newevent = new User(req.event);
-
-        if (address) {
-            event.address = address;
-        }
-        Event.findByIdAndUpdate(req.params.id,
-            newEvent,
-            {new: true, runValidators: true},
-            function (err, event) {
-                if (err) {
-                    res.status(500).json({message: err.name || err.message || "Unknown server error", data: []});
-                } else if (!user) {
-                    res.status(404).json({message: "Event not found", data: []});
-                } else {
-                    res.status(200).json({message: "Event updated", data: user});
-                }
-            });
+    router.put('/users/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        updateResource(User, req, res);
     });
 
-    router.delete(function (req, res) {
-        User.findByIdAndRemove(req.params.user_id,
-            function (err, user) {
-                if (err || !user) {
-                    res.status(404).json({message: "User not found", data: []});
-                } else {
-                    res.status(200).json({message: "User deleted", data: user});
-                }
-            });
+    router.put('/events/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        updateResource(Event, req, res);
+    });
+
+    router.put('/addresses/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        updateResource(Address, req, res);
+    });
+
+    router.put('/images/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        updateResource(Image, req, res);
+    });
+
+    router.put('/invite/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        updateResource(Invite, req, res);
     });
 
 
@@ -215,6 +224,56 @@ module.exports = function (router, passport) {
 
     router.get('/events', passport.authenticate('jwt', {session: false}), function (req, res) {
         getResources(Event, req, res);
+    });
+
+    router.get('/addresses', passport.authenticate('jwt', {session: false}), function (req, res) {
+        getResources(Address, req, res);
+    });
+
+    router.get('/invites', passport.authenticate('jwt', {session: false}), function (req, res) {
+        getResources(Invite, req, res);
+    });
+
+    // get single resource
+    router.get('/users/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        getSingleResource(User, req, res);
+    });
+
+    router.get('/events/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        getSingleResource(Event, req, res);
+    });
+
+    router.get('/addresses/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        getSingleResource(Address, req, res);
+    });
+
+    router.get('/invites/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        getSingleResource(Invite, req, res);
+    });
+
+    router.get('/images/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        getSingleResource(Image, req, res);
+    });
+
+    // delete single resource
+    router.delete('/users/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        deleteResource(User, req, res);
+    });
+
+    router.delete('/events/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        deleteResource(Event, req, res);
+    });
+
+    router.delete('/addresses/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        deleteResource(Address, req, res);
+    });
+
+    router.delete('/invites/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        deleteResource(Invite, req, res);
+    });
+
+    router.delete('/images/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+        deleteResource(Image, req, res);
     });
 
 
@@ -242,30 +301,4 @@ module.exports = function (router, passport) {
         res.writeHead(200);
         res.end();
     });
-
-    //router.get('/users', passport.authenticate('jwt', {session: false}), function (req, res) {
-    //    var where = eval("(" + req.query.where + ")");
-    //    var sort = eval("(" + req.query.sort + ")");
-    //    var select = eval("(" + req.query.select + ")");
-    //    var skip = eval("(" + req.query.skip + ")");
-    //    var limit = eval("(" + req.query.limit + ")");
-    //    var count = eval("(" + req.query.count + ")");
-    //    var document = User.find(where);
-    //
-    //    if (count) {
-    //        document.count(function (err, count) {
-    //            if (err) {
-    //                res.status(500).json({message: err.name || err.message, data: []});
-    //            } else {
-    //                res.status(200).json({message: "OK", data: count});
-    //            }
-    //        });
-    //    } else {
-    //        document.sort(sort).skip(skip).limit(limit).select(select).exec().then(function (user) {
-    //            res.status(200).json({message: "OK", data: user});
-    //        }, function (err) {
-    //            res.status(500).json({message: err.name || err.message || "Unknown Internal Server Error", data: []});
-    //        });
-    //    }
-    //});
 };
